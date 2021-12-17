47dcd30b4b6c5cd1e00c2ea5466203c0d5ac553b
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2021-12-18 00:00:04 +0100

    Initial Commit

---
+++ LearnSwift/LearnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftApp.swift
+//  LearnSwift
+//
+//  Created by Cristina on 2021-12-17.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
